1ec2156309a7d3e1ff4047fb541850d80d13bc0f
Gm Fredricksen <xgiuliano.morox@gmail.com>
2018-05-23 00:54:45 +0200

    initial html structure for the homepage
    Why?
    *
    
    Issue:
    *   #1

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,213 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Badge from '@material-ui/core/Badge';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import CommentIcon from '@material-ui/icons/Comment';
+import MenuIcon from '@material-ui/icons/Menu';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AddPostIcon from '@material-ui/icons/PlaylistAdd';
+import FaceIcon from '@material-ui/icons/Face';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import classnames from 'classnames';
 import './App.css';
 
-const App = () => (
-  <div>
-    <h1>Readable</h1>
-  </div>
-)
+const drawerWidth = 240;
+const styles = (theme) => ({
+  appBar: {
+    display: 'flex',
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
+  navBarTitle: {
+    flex: 1,
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  navIconAddPost: {
+    
+  },
+  paper: {
+    padding: '15px',
+  },
+  root: {
+    flexGrow: 1,
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  toolbar: theme.mixins.toolbar,
+})
 
-export default App;
+class App extends Component {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  }
+
+  render() {
+    const { classes, theme } = this.props;
+    const { mobileOpen } = this.state;
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar}/>
+        <Divider />
+        <List component="nav">
+          <ListItem button component="a" href="/">
+            <ListItemText primary="All" />
+          </ListItem>
+          <Divider />
+          <ListItem button component="a" href="/1">
+            <ListItemText primary="Category 1" />
+          </ListItem>
+          <Divider />
+          <ListItem button component="a" href="/2">
+            <ListItemText primary="Category 2" />
+          </ListItem>
+        </List>
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color='inherit'
+              aria-label='open drawer'
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='title' color='inherit' className={classes.navBarTitle} noWrap>
+              GM - Readable
+            </Typography>
+            <IconButton
+              color='inherit'
+              aria-label='add post'
+              className={classes.navIconAddPost}>
+              <AddPostIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor="left"
+            open={mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Grid container justify='center' spacing={8}>
+            <Grid item xs={12}>
+              
+              <Card className={classes.card}>
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="Recipe" className={classes.avatar}>
+                      <FaceIcon />
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title="Post Title Here"
+                  subheader="September 14, 2016"
+                />
+                <CardContent>
+                  <Typography component="p">
+                    Post Content Here
+                  </Typography>
+                </CardContent>
+                <CardActions className={classes.actions} disableActionSpacing>
+                  <IconButton aria-label="Vote">
+                    {/* <VoteMechanism /> */}
+                  </IconButton>
+                  <IconButton aria-label="Comments">
+                    <Badge badgeContent={4} color="secondary">
+                      <CommentIcon />
+                    </Badge>
+                  </IconButton>
+                  <IconButton
+                    className={classnames(classes.expand, {
+                      [classes.expandOpen]: this.state.expanded,
+                    })}
+                    onClick={this.handleExpandClick}
+                    aria-expanded={this.state.expanded}
+                    aria-label="Edit"
+                  >
+                    {/* <Edit - Delete /> */}
+                  </IconButton>
+                </CardActions>
+              </Card>
+
+            </Grid>
+          </Grid>
+        </main>
+      </div>
+    );
+  }
+};
+
+export default withStyles(styles)(App);
