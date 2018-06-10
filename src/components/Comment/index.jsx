9f370084b4671b0742aafdd7775b2546b57902b5
GmFredricksen <xgiuliano.morox@gmail.com>
2018-06-10 14:21:12 +0200

    feat: update Comment component to use the passed in data
    Why?
    *
    
    Issue:
    *   #3

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import fecha from 'fecha';
 
 import MenuOfActionsOnEntity from '../MenuOfActionsOnEntity';
 import VotingSystem from '../VotingSystem';
 
-class Comment extends Component {
-  render() {
-    return (
-      <ListItem divider>
-        <VotingSystem />
-        <ListItemText
-          primary="1st Comment"
-          secondary="Author - September 14, 2016"
-        />
-        <MenuOfActionsOnEntity />
-      </ListItem>
-    );
-  }
-}
+const Comment = ({ comment }) => (
+  <ListItem divider>
+    <VotingSystem voteScore={comment.voteScore} />
+    <ListItemText
+      primary={comment.body}
+      secondary={`${comment.author} - ${fecha.format(comment.timestamp, 'mediumDate')}`}
+    />
+    <MenuOfActionsOnEntity />
+  </ListItem>
+);
 
 export default Comment;
