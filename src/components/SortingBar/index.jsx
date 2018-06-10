83cc1ac1cd6e36b8020b6c41e181e9eef8ad29a6
GmFredricksen <xgiuliano.morox@gmail.com>
2018-06-10 17:31:12 +0200

    refactor: implement propTypes in SortingBar component
    Why?
    *
    
    Issue:
    *   #3

--- src/components/SortingBar/index.jsx
+++ src/components/SortingBar/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
@@ -40,5 +41,9 @@ class SortingBar extends Component {
     );
   }
 }
+
+SortingBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(SortingBar);
