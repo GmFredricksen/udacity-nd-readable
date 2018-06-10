2d3954fa2e75b4ea50cf827b70fa7328c56acc44
GmFredricksen <xgiuliano.morox@gmail.com>
2018-06-10 17:29:51 +0200

    refactor: implement propTypes in PostModal component
    Why?
    *
    
    Issue:
    *   #3

--- src/components/PostModal/index.jsx
+++ src/components/PostModal/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Typography from '@material-ui/core/Typography';
@@ -16,8 +16,8 @@ const styles = (theme) => ({
   addPostModal: {
     position: 'absolute',
     top: '30%',
-    left: `calc(50% - ${modalWidth/2}px)`,
-    transform: `translate(-30%, -calc(50% - ${modalWidth/2}px))`,
+    left: `calc(50% - ${modalWidth / 2}px)`,
+    transform: `translate(-30%, -calc(50% - ${modalWidth / 2}px))`,
     width: modalWidth,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
@@ -33,61 +33,67 @@ class PostModal extends Component {
     const { classes, handleCloseCreatePostModal, isCreatePostModalOpen } = this.props;
 
     return (
-        <Modal
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-          open={isCreatePostModalOpen}
-          onClose={handleCloseCreatePostModal}
-        >
-          <div className={classes.addPostModal}>
-            <Typography variant="title" id="modal-title">
-              Add new Post
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={isCreatePostModalOpen}
+        onClose={handleCloseCreatePostModal}
+      >
+        <div className={classes.addPostModal}>
+          <Typography variant="title" id="modal-title">
+            Add new Post
             </Typography>
-            
-            <form className={classes.container} autoComplete="off">
-              <FormGroup>
-                <TextField
-                  id="postTitle"
-                  label="Post Title"
-                  className={classes.textField}
-                  margin="normal"
-                />
-              </FormGroup>
-              <FormGroup>
-                <TextField
-                  id="postMessage"
-                  label="Message"
-                  className={classes.textField}
-                  margin="normal"
-                  multiline
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormControl className={classes.formControl}>
-                  <InputLabel htmlFor="age-simple">Category</InputLabel>
-                  <Select value={0}>
-                    <MenuItem value={1}>Category 1</MenuItem>
-                    <MenuItem value={2}>Category 2</MenuItem>
-                  </Select>
-                </FormControl>
-              </FormGroup>
-              <FormGroup>
-                <Button
-                  className={classes.button}
-                  color="default"
-                  onClick={handleCloseCreatePostModal}
-                >
-                  Cancel
+
+          <form className={classes.container} autoComplete="off">
+            <FormGroup>
+              <TextField
+                id="postTitle"
+                label="Post Title"
+                className={classes.textField}
+                margin="normal"
+              />
+            </FormGroup>
+            <FormGroup>
+              <TextField
+                id="postMessage"
+                label="Message"
+                className={classes.textField}
+                margin="normal"
+                multiline
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-simple">Category</InputLabel>
+                <Select value={0}>
+                  <MenuItem value={1}>Category 1</MenuItem>
+                  <MenuItem value={2}>Category 2</MenuItem>
+                </Select>
+              </FormControl>
+            </FormGroup>
+            <FormGroup>
+              <Button
+                className={classes.button}
+                color="default"
+                onClick={handleCloseCreatePostModal}
+              >
+                Cancel
                 </Button>
-                <Button className={classes.button} color="primary">
-                  Add
+              <Button className={classes.button} color="primary">
+                Add
                 </Button>
-              </FormGroup>
-            </form>
-          </div>
-        </Modal>
+            </FormGroup>
+          </form>
+        </div>
+      </Modal>
     );
   }
 }
+
+PostModal.propTypes = {
+  classes: PropTypes.object.isRequired,
+  handleCloseCreatePostModal: PropTypes.func.isRequired,
+  isCreatePostModalOpen: PropTypes.bool.isRequired,
+};
 
 export default withStyles(styles)(PostModal);
