3e4d0cabef38adeb38de6f52369ceed0d3615d9c
GmFredricksen <xgiuliano.morox@gmail.com>
2018-06-11 02:16:59 +0200

    feat: implement SET_POST and UPDATE_POST_VOTE in reducers
    Why?
    *
    
    Issue:
    *   #3

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,16 @@
+/* eslint no-case-declarations: 0 */
+
 import { combineReducers } from 'redux';
 
 import {
+  UPDATE_POST_VOTE,
   SET_CATEGORIES,
   SET_COMMENTS_FOR_POST,
   SET_POSTS,
+  SET_POST,
 } from '../actions';
 
-function categories (state = {}, action) {
+function categories (state = [], action) {
   switch (action.type) {
     case SET_CATEGORIES:
       const { categories } = action;
@@ -17,12 +21,23 @@ function categories (state = {}, action) {
   }
 }
 
-function posts (state = {}, action) {
+function posts (state = [], action) {
   switch (action.type) {
     case SET_POSTS:
       const { posts } = action;
 
       return posts;
+    case SET_POST:
+      const { post } = action;
+
+      return [ post ];
+    case UPDATE_POST_VOTE:
+      const { postId, voteScore } = action;
+
+      return state.map((post) => {
+        return post.id === postId ?
+          { ...post, voteScore } : post
+      });
     default :
       return state
   }
