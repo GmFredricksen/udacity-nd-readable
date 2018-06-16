2b6873e3d667b1608c42c88b25129c009c05cfbd
GmFredricksen <xgiuliano.morox@gmail.com>
2018-06-16 18:35:29 +0200

    feat: implement voting functionality for Comments
    Why?
    *
    
    Issue:
    *   #3

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import fecha from 'fecha';
 
+import * as ReadableAPI from '../../utils/ReadableAPI';
+import { updateCommentVote } from '../../actions';
 import MenuOfActionsOnEntity from '../MenuOfActionsOnEntity';
 import VotingSystem from '../VotingSystem';
 
-const Comment = ({ comment }) => (
+const Comment = ({ comment, updateCommentVote }) => (
   <ListItem divider>
-    <VotingSystem voteScore={comment.voteScore} />
+    <VotingSystem
+      updateVote={updateCommentVote}
+      voteScore={comment.voteScore}
+    />
     <ListItemText
       primary={comment.body}
       secondary={`${comment.author} - ${fecha.format(comment.timestamp, 'mediumDate')}`}
@@ -20,6 +26,19 @@ const Comment = ({ comment }) => (
 
 Comment.propTypes = {
   comment: PropTypes.object.isRequired,
+  updateCommentVote: PropTypes.func.isRequired,
 };
 
-export default Comment;
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    updateCommentVote: (voteScore) => {
+      ReadableAPI.updateCommentVote(ownProps.comment.id, voteScore)
+        .then((comment) => dispatch(updateCommentVote(comment.id, comment.parentId, comment.voteScore)));
+    },
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Comment);
