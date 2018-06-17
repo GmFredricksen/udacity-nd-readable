264ebebcea1e2050f55692027bb27b19f0d1ff61
Gm Fredricksen <xgiuliano.morox@gmail.com>
2018-06-17 21:35:47 +0200

    feat: implement add post functionality
    Why?
    *
    
    Issue:
    *   #3

--- src/components/PostModal/index.jsx
+++ src/components/PostModal/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
@@ -10,6 +11,9 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
+
+import { addPost } from '../../actions';
+import * as ReadableAPI from '../../utils/ReadableAPI';
 
 const modalWidth = 500;
 const styles = (theme) => ({
@@ -27,10 +31,30 @@ const styles = (theme) => ({
 
 class PostModal extends Component {
   state = {
+    postToBeSubmitted: {
+      author: '',
+      body: '',
+      category: '',
+      title: '',
+    }
+  }
+
+  formIsValid = ({ author, body, category, title }) => (
+    author.length && body.length && category.length && title.length
+  )
+
+  handleChangeOfFormInputs = (event) => {
+    this.setState({
+      postToBeSubmitted: {
+        ...this.state.postToBeSubmitted,
+        [event.target.name]: event.target.value,
+      }
+    });
   }
 
   render() {
-    const { classes, handleCloseCreatePostModal, isCreatePostModalOpen } = this.props;
+    const { submitPost, categories, classes, handleCloseCreatePostModal, isCreatePostModalOpen } = this.props;
+    const { postToBeSubmitted } = this.state;
 
     return (
       <Modal
@@ -44,13 +68,27 @@ class PostModal extends Component {
             Add new Post
             </Typography>
 
-          <form className={classes.container} autoComplete="off">
+          <form className={classes.container} autoComplete="off" onSubmit={(event) => submitPost(event, postToBeSubmitted)}>
             <FormGroup>
               <TextField
                 id="postTitle"
                 label="Post Title"
                 className={classes.textField}
                 margin="normal"
+                name="title"
+                value={postToBeSubmitted.title}
+                onChange={this.handleChangeOfFormInputs}
+              />
+            </FormGroup>
+            <FormGroup>
+              <TextField
+                id="postAuthor"
+                label="Post Author"
+                className={classes.textField}
+                margin="normal"
+                name="author"
+                value={postToBeSubmitted.author}
+                onChange={this.handleChangeOfFormInputs}
               />
             </FormGroup>
             <FormGroup>
@@ -60,14 +98,21 @@ class PostModal extends Component {
                 className={classes.textField}
                 margin="normal"
                 multiline
+                name="body"
+                value={postToBeSubmitted.body}
+                onChange={this.handleChangeOfFormInputs}
               />
             </FormGroup>
             <FormGroup>
               <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="age-simple">Category</InputLabel>
-                <Select value={0}>
-                  <MenuItem value={1}>Category 1</MenuItem>
-                  <MenuItem value={2}>Category 2</MenuItem>
+                <InputLabel htmlFor="postCategory">Category</InputLabel>
+                <Select
+                  id="postCategory"
+                  name="category"
+                  value={postToBeSubmitted.category || 0}
+                  onChange={this.handleChangeOfFormInputs}
+                >
+                  {categories.map(({ name }) => <MenuItem key={name} value={name}>{name}</MenuItem>)}
                 </Select>
               </FormControl>
             </FormGroup>
@@ -78,10 +123,15 @@ class PostModal extends Component {
                 onClick={handleCloseCreatePostModal}
               >
                 Cancel
-                </Button>
-              <Button className={classes.button} color="primary">
+              </Button>
+              <Button
+                className={classes.button}
+                color="primary"
+                type="submit"
+                disabled={!this.formIsValid(postToBeSubmitted)}
+              >
                 Add
-                </Button>
+              </Button>
             </FormGroup>
           </form>
         </div>
@@ -91,9 +141,27 @@ class PostModal extends Component {
 }
 
 PostModal.propTypes = {
+  categories: PropTypes.array.isRequired,
   classes: PropTypes.object.isRequired,
   handleCloseCreatePostModal: PropTypes.func.isRequired,
   isCreatePostModalOpen: PropTypes.bool.isRequired,
+  submitPost: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(PostModal);
+const mapStateToProps = ({ categories }) => ({
+  categories,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  submitPost: (formSubmitEvent, postToBeSubmitted) => {
+    formSubmitEvent.preventDefault();
+
+    ReadableAPI.addPost(postToBeSubmitted)
+      .then((post) => dispatch(addPost(post)));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withStyles(styles)(PostModal));
